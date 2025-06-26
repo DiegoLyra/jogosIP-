50dc830c4fcf48399cbd0c7c4e6406d524cf562c
Diego Lyra <diego.lyra@upe.br>
2025-06-26 15:13:55 -0300

    pisando_em_ovosIP.py

--- pisando_em_ovosIP.py
+++ pisando_em_ovosIP.py
@@ -8,6 +8,9 @@ def menu():
 def verificar(possivel_entrada, opcao):
     while opcao not in possivel_entrada:
         opcao = int(input("Entrada inválida. Digite uma nova entrada\n"))
+    if opcao == 2 or opcao == 3:
+        if armador == 0 or andarilho == 0:
+            opcao = int(input("Primeiro, defina os jogadores\n"))
     return opcao
 
 def terreno():
@@ -31,35 +34,40 @@ def plantar_armadilhas():
             coluna = verificar((0,1,2,3,4), coluna)
             ter[i][coluna] = "O"
     return qnt, coluna
-   
+
 def andar_andarilho(x, possibilidades):
-    global pontos_and, pontos_arm
+    andar = int(input("São validos os espaços:{}\nEscolha sabiamente um dos espaços válidos\n".format(possibilidades)))
+    andar = verificar(possibilidades, andar)
+    possibilidades = [0,1,2,3,4]
     while x < 5:
-        andar = int(input("São validos os espaços:{}\nEscolha sabiamente um dos espaços válidos\n".format(possibilidades)))
         andar-= 1
-        possibilidades = (0,1,2,3,4)
-        andar = verificar(possibilidades, andar)
         if ter[x][andar] == "O":
             print("Eca! Você pisou em um ovo podre e perdeu")
-            pontos_arm += 1
+            placar(armador)
             return
         elif andar == 0:
-            possibilidades = (1,2)
-            andar = verificar(possibilidades, andar)
+            possibilidades = [1,2]
         elif andar == 4:
-            possibilidades = (4,5)
-            andar = verificar(possibilidades, andar)
+            possibilidades = [4,5]
         else:
             possibilidades = [andar, andar+1, andar+2]
-            andar = verificar(possibilidades, andar)
+        andar = int(input("São validos os espaços:{}\nEscolha sabiamente um dos espaços válidos\n".format(possibilidades)))
+        andar = verificar(possibilidades, andar)
         x += 1
     print("Você atravessou o terreno sem cair em nenhuma armadilha! Parabéns!!!")
-    pontos_and += 1
+    placar(andarilho)
+    return possibilidades
 
+def placar(personagem):
+    if personagem == jogador1['numero']: 
+        jogador1['pontos'] += 1
+    else:
+        jogador2['pontos'] += 1
+
+jogador1 = {'numero' : 1, 'pontos': 0}
+jogador2 = {'numero' : 2, 'pontos': 0}
 andarilho = 0
 armador = 0
-pontos_and = 0
-pontos_arm = 0
 coluna = 0
 ter = []
 
@@ -71,30 +79,26 @@ while entrada != 0:
         selecao = int(input("Qual jogador plantará as armadilhas?\n"))
         selecao = verificar((1,2), selecao)
         armador = selecao
-        andarilho = 2 if armador == 1 else 1
+        andarilho = jogador2["numero"] if armador == jogador1['numero'] else jogador1['numero']
         print("O armador é o jogador: {} \nO andarilho é o jogador: {}".format(armador, andarilho))
+
     elif entrada == 2:
-        if armador == 0 or andarilho == 0:
-            print("Primeiro, defina os jogadores")
-        else:
-            print("O terreno base é: ")
-            ter = terreno()
-            for linha in ter:
-                print(*linha)
-            plantar_armadilhas()
-            for linha in ter:
-                print(*linha)
+        print("O terreno base é: ")
+        ter = terreno()
+        for linha in ter:
+            print(*linha)
+        plantar_armadilhas()
+        for linha in ter:
+            print(*linha)
+
     elif entrada == 3:
-        if armador == 0 or andarilho == 0:
-            print("Primeiro, defina os jogadores")
-        else:
-            for x in range(100):
-                print("=" * x)
-            linha = 0
-            espacos = [1,2,3,4,5]
-            andar_andarilho(linha, espacos)
+        for x in range(100):
+            print("=" * x)
+        linha = 0
+        espacos = andar_andarilho(0, [1,2,3,4,5])
+
     elif entrada == 4:
-        print("Pontuação do jogador {}: {}".format(andarilho, pontos_and))
-        print("Pontuação do jogador {}: {}".format(armador, pontos_arm))
+        print("Pontuação do jogador {}: {}".format(jogador1["numero"], jogador1['pontos']))
+        print("Pontuação do jogador {}: {}".format(jogador2["numero"], jogador2['pontos']))
     menu()
     entrada = int(input())
